f9f7e5f9b2ff2e8ae2815079ed0df16af2b9ab14
kamrulhasananik019 <kamrulhasananik019@gmail.com>
2024-09-06 19:01:48 +0600

    class add

---
+++ OOP.java
@@ -0,0 +1,10 @@
+class  OOP{
+    public static void main(String a[])
+    {
+        int num1 = 6;
+        int num2 = 9;
+        int result= num1 + num2;
+        System.out.println(result);
+    // System.out.print(4+5);
+    }
+}
